ca5bb55744188e0914056c4676abc376f6d3347c
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-11 16:33:39 +0200

    adding helmet and compression packages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: './src/environment_variables.env' });
 // const config = require('config');
 const express = require('express');
-const helmet = require('helmet')
-const cors = require('cors')
 const debug = require('debug')('app:startup');
 const app = express();
 const sequelize = require('../utils/database');
@@ -12,8 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.use("/api/images", express.static('images'));
-app.use(helmet());
-app.use(cors());
+
+require('../startup/prod')(app)
 require('../startup/routes')(app)
 
 const port = process.env.PORT || 3000
