7486c44e3212311300450295a8fb3377cf21cada
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-12 13:50:59 +0200

    update the start script

--- utils/database.js
+++ utils/database.js
@@ -1,20 +1,6 @@
 const { Sequelize } = require('sequelize');
 const config = require('config');
 
-// const sequelize = new Sequelize(
-//     'lego_schema',
-//     'root',
-//     process.env.dbPass,
-//     {
-//         logging: process.env.NODE_ENV == "development" ? true : false,
-//         dialect: 'mysql',
-//         host: 'localhost'
-//     }
-// );
-
-// module.exports = sequelize;
-
-
 const sequelize = new Sequelize(
     'defaultdb',
     'avnadmin',
