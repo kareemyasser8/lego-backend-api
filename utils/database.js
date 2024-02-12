d7975f09ba03575ac8c5662c8c2b9006e1118720
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-12 13:35:14 +0200

    update the connection to the new deployed database

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,29 @@
 const { Sequelize } = require('sequelize');
 const config = require('config');
 
+// const sequelize = new Sequelize(
+//     'lego_schema',
+//     'root',
+//     process.env.dbPass,
+//     {
+//         logging: process.env.NODE_ENV == "development" ? true : false,
+//         dialect: 'mysql',
+//         host: 'localhost'
+//     }
+// );
+
+// module.exports = sequelize;
+
+
 const sequelize = new Sequelize(
-    'lego_schema',
-    'root',
+    'defaultdb',
+    'avnadmin',
     process.env.dbPass,
     {
         logging: process.env.NODE_ENV == "development" ? true : false,
         dialect: 'mysql',
-        host: 'localhost'
+        host: 'mysql-306e5a9d-lego-clone-api.a.aivencloud.com:17514'
     }
 );
 
 module.exports = sequelize;
-
