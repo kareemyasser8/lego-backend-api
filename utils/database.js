0401f97f2c2743933b3eb08787f453a6aca41d68
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-13 14:13:35 +0200

    add mysql2 module in the sequelize configuration

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const config = require('config');
+const mysql2 = require('mysql2')
 
 const sequelize = new Sequelize(
     process.env.dbName,
@@ -8,6 +9,7 @@ const sequelize = new Sequelize(
     {
         // logging: process.env.NODE_ENV == "development" ? true : false,
         dialect: 'mysql',
+        dialectModule: mysql2,
         host: 'mysql-306e5a9d-lego-clone-api.a.aivencloud.com',
         port: process.env.dbPORT
     }
