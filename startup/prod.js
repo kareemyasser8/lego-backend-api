52aa9d9c8b61cad0449d90c5e6dc87a603458fcb
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-13 14:19:22 +0200

    removing uncessary log file

--- startup/prod.js
+++ startup/prod.js
@@ -1,19 +1,19 @@
 const helmet = require('helmet')
 const cors = require('cors')
 const compression = require('compression');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
+// const morgan = require('morgan');
+// const fs = require('fs');
+// const path = require('path');
 
 module.exports = (app) => {
 
-    const accessLogStream = fs.createWriteStream(
-        path.join(__dirname, 'access.log'),
-        { flags: 'a' }
-    )
+    // const accessLogStream = fs.createWriteStream(
+    //     path.join(__dirname, 'access.log'),
+    //     { flags: 'a' }
+    // )
 
     app.use(helmet());
     app.use(cors());
     app.use(compression());
-    app.use(morgan('combined', { stream: accessLogStream }));
+    // app.use(morgan('combined', { stream: accessLogStream }));
 }
