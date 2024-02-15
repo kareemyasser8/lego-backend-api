5cd011072e16f19e3351b7f28f8320975bf859ed
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-15 22:07:28 +0200

    bugFix: update the CORS and remove slashes

--- startup/prod.js
+++ startup/prod.js
@@ -3,9 +3,10 @@ const cors = require('cors')
 const compression = require('compression');
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://lego-project-pi.vercel.app/'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
+  origin: ['http://localhost:5173', 'https://lego-project-pi.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  optionsSuccessStatus: 200
+};
   
 module.exports = (app) => {
     app.use(cors(corsOptions));
