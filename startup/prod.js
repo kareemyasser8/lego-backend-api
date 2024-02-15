24472e18fa5b9f319c32c56a79a53808d5034d36
kareemyasser8 <kareem.yasserr@gmail.com>
2024-02-15 21:59:43 +0200

    bugFix: update the cors

--- startup/prod.js
+++ startup/prod.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const compression = require('compression');
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://lego-backend-api.vercel.app'],
+    origin: ['http://localhost:5173', 'https://lego-project-pi.vercel.app/'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
   
